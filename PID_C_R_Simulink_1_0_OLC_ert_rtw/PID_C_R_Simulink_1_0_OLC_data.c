a677542e6451ab574a87ea00adf80a42657f6689
Mohanad M. Marzouk <52548074+Mohanad-M@users.noreply.github.com>
2021-05-08 21:43:26 +0200

    Open Loop SIM
    __Finalized open loop control model

---
+++ PID_C_R_Simulink_1_0_OLC_ert_rtw/PID_C_R_Simulink_1_0_OLC_data.c
@@ -0,0 +1,86 @@
+/*
+ * File: PID_C_R_Simulink_1_0_OLC_data.c
+ *
+ * Code generated for Simulink model 'PID_C_R_Simulink_1_0_OLC'.
+ *
+ * Model version                  : 1.8
+ * Simulink Coder version         : 9.3 (R2020a) 18-Nov-2019
+ * C/C++ source code generated on : Sat May  8 21:23:49 2021
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Atmel->AVR
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#include "PID_C_R_Simulink_1_0_OLC.h"
+#include "PID_C_R_Simulink_1_0_OLC_private.h"
+
+/* Block parameters (default storage) */
+P_PID_C_R_Simulink_1_0_OLC_T PID_C_R_Simulink_1_0_OLC_P = {
+  /* Expression: 0.001
+   * Referenced by: '<S5>/Digital Input'
+   */
+  0.001,
+
+  /* Computed Parameter: Outport_Y0
+   * Referenced by: '<S10>/Outport'
+   */
+  0.0,
+
+  /* Expression: 0.001
+   * Referenced by: '<S7>/Digital Input'
+   */
+  0.001,
+
+  /* Computed Parameter: Outport_Y0_h
+   * Referenced by: '<S11>/Outport'
+   */
+  0.0,
+
+  /* Expression: 75
+   * Referenced by: '<Root>/Constant'
+   */
+  75.0,
+
+  /* Expression: 0
+   * Referenced by: '<Root>/Constant1'
+   */
+  0.0,
+
+  /* Expression: 2.55
+   * Referenced by: '<S3>/Scale'
+   */
+  2.55,
+
+  /* Expression: 2.55
+   * Referenced by: '<S3>/Scale1'
+   */
+  2.55,
+
+  /* Expression: 0
+   * Referenced by: '<S6>/Constant'
+   */
+  0.0,
+
+  /* Expression: 1
+   * Referenced by: '<S6>/Constant1'
+   */
+  1.0,
+
+  /* Expression: 0
+   * Referenced by: '<S6>/Constant2'
+   */
+  0.0,
+
+  /* Expression: 1
+   * Referenced by: '<S6>/Constant3'
+   */
+  1.0
+};
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
---
+++ PID_C_R_Simulink_1_0_OLC_ert_rtw/PID_C_R_Simulink_1_0_OLC_private.h
@@ -0,0 +1,29 @@
+/*
+ * File: PID_C_R_Simulink_1_0_OLC_private.h
+ *
+ * Code generated for Simulink model 'PID_C_R_Simulink_1_0_OLC'.
+ *
+ * Model version                  : 1.8
+ * Simulink Coder version         : 9.3 (R2020a) 18-Nov-2019
+ * C/C++ source code generated on : Sat May  8 21:23:49 2021
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Atmel->AVR
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_PID_C_R_Simulink_1_0_OLC_private_h_
+#define RTW_HEADER_PID_C_R_Simulink_1_0_OLC_private_h_
+#include "rtwtypes.h"
+#include "zero_crossing_types.h"
+
+extern real_T rt_roundd_snf(real_T u);
+
+#endif                      /* RTW_HEADER_PID_C_R_Simulink_1_0_OLC_private_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
